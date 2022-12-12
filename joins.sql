2e72ab130d2b08ee8986c94d18abd0c78db7e390
weston-bailey <wdbailey87@gmail.com>
2022-12-12 11:25:47 -0800

    finish lesson

--- joins.sql
+++ joins.sql
@@ -7,10 +7,14 @@
 
 
 -- aliases, create a shorthand variable name for table names
-SELECT c.name, c.id, o.order_num FROM customers c 
-FULL OUTER JOIN orders o ON c.id = o.customer_id;
+-- SELECT c.name, c.id, o.order_num FROM customers c 
+-- FULL OUTER JOIN orders o ON c.id = o.customer_id
+-- WHERE LOWER(c.name) LIKE 'w%';
 
 -- nested query for adding relational data
-INSERT INTO orders (order_num, amount, customer_id) VALUES ('Xt4fr23', 5000, (
-    SELECT id FROM customers WHERE name = 'Weston'
-));
+-- INSERT INTO orders (order_num, amount, customer_id) VALUES ('Xt4fr23', 5000, (
+--     SELECT id FROM customers WHERE name = 'Weston'
+-- ));
+
+-- AGGREGATE FUNCTIONS
+SELECT MIN(salary) FROM customers;
